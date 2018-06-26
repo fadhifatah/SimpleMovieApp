952c9f26d3b7e8a24d55a951f0efdc4e702148ab
Fatah Fadhlurrohman <fadhlurrohman.nf@gmail.com>
2018-06-26 21:26:53 +0700

    Update detail, checkpoint #4, Stable

--- app/src/main/java/com/fadhifatah/omdbapp/activity/DetailActivity.java
+++ app/src/main/java/com/fadhifatah/omdbapp/activity/DetailActivity.java
@@ -1,6 +1,8 @@
 package com.fadhifatah.omdbapp.activity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,11 +14,8 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.fadhifatah.omdbapp.R;
 import com.fadhifatah.omdbapp.adapter.RatingAdapter;
 import com.fadhifatah.omdbapp.model.MovieModel;
-import com.fadhifatah.omdbapp.model.RatingModel;
 import com.fadhifatah.omdbapp.service.API;
 import com.fadhifatah.omdbapp.util.Constant;
-
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,7 +24,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class DetailActivity extends AppCompatActivity {
-    private List<RatingModel> list;
+    private ProgressDialog dialog;
 
     @BindView(R.id.poster_detail)
     ImageView poster;
@@ -39,17 +38,35 @@ public class DetailActivity extends AppCompatActivity {
     @BindView(R.id.additional_detail)
     TextView additional;
 
+    @BindView(R.id.scores)
+    TextView scores;
+
     @BindView(R.id.directed)
     TextView director;
 
+    @BindView(R.id.awards)
+    TextView awards;
+
     @BindView(R.id.writer)
     TextView writer;
+
+    @BindView(R.id.actor)
+    TextView actor;
 
     @BindView(R.id.rating_detail)
     RecyclerView ratingView;
 
     @BindView(R.id.plot_detail)
     TextView plot;
+
+    @BindView(R.id.dvd)
+    TextView dvd;
+
+    @BindView(R.id.website)
+    TextView website;
+
+    @BindView(R.id.box_office)
+    TextView boxOffice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +78,13 @@ public class DetailActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(imdbId);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Loading...");
+        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        dialog.setIndeterminate(false);
+        dialog.setCancelable(false);
+        dialog.show();
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         ratingView.setLayoutManager(layoutManager);
@@ -88,14 +112,24 @@ public class DetailActivity extends AppCompatActivity {
                             information.setText(s);
 
                             s = "Released at " + model.released + " from " + model.country + " (" +
-                                    model.language + ")";
+                                    model.language + ") • " + model.production;
                             additional.setText(s);
+
+                            s = "Metascore - " + model.metascore + " • IMDb Rating - " + model.imdbRating +
+                                    " • " + model.imdbVotes + " votes";
+                            scores.setText(s);
 
                             s = "Directed by " + model.director;
                             director.setText(s);
 
-                            s = "Writer: " + model.writer;
-                            writer.setText(s);
+                            s = "Awards - " + model.awards;
+                            awards.setText(s);
+
+                            writer.setText(model.writer);
+                            actor.setText(model.actors);
+                            dvd.setText(model.dvd);
+                            website.setText(model.website);
+                            boxOffice.setText(model.boxOffice);
 
                             s = "\"" + model.plot + "\"";
                             plot.setText(s);
@@ -103,11 +137,16 @@ public class DetailActivity extends AppCompatActivity {
                             RatingAdapter adapter = new RatingAdapter(getBaseContext(), model.ratingList);
                             ratingView.setAdapter(adapter);
                         }
+                        else {
+                            Snackbar.make(ratingView, model.error, Snackbar.LENGTH_SHORT).show();
+                        }
+                        dialog.dismiss();
                     }
 
                     @Override
                     public void onFailure(Call<MovieModel> call, Throwable t) {
-
+                        dialog.dismiss();
+                        Snackbar.make(ratingView, "Time out! " + t.getMessage(), Snackbar.LENGTH_SHORT).show();
                     }
                 });
     }
--- app/src/main/java/com/fadhifatah/omdbapp/activity/MainActivity.java
+++ app/src/main/java/com/fadhifatah/omdbapp/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fadhifatah.omdbapp.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -10,7 +11,9 @@ import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
@@ -33,12 +36,17 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity implements ItemListener {
+    private final String STATE_LIST = "state_list";
+    private final String STATE_INDEX = "state_index";
+    private final String STATE_LOADING = "state_loading";
+
     private int INDEX = 1;
     private RecyclerView.LayoutManager layoutManager;
     private ItemPresenter presenter;
     private String query, type, year;
     private int pastVisibleItem, visibleItemCount, totalItemCount;
     private boolean isLoading = true;
+    private List<ItemModel> list = new ArrayList<>();
 
     @BindView(R.id.year)
     EditText yearET;
@@ -62,7 +70,27 @@ public class MainActivity extends AppCompatActivity implements ItemListener {
         ButterKnife.bind(this);
 
         presenter = new ItemPresenter(this);
+
+        searchET.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    submitIB.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
         setUpLayoutManager();
+
+        if (savedInstanceState != null) {
+            list = new ArrayList<>((List<ItemModel>) savedInstanceState.getSerializable(STATE_LIST));
+            ItemAdapter adapter = new ItemAdapter(this, list);
+            recyclerView.setAdapter(adapter);
+
+            INDEX = savedInstanceState.getInt(STATE_INDEX);
+            isLoading = true;
+        }
     }
 
     private void setUpLayoutManager() {
@@ -71,7 +99,7 @@ public class MainActivity extends AppCompatActivity implements ItemListener {
             spanCount = 2;
         }
         else {
-            spanCount = 3;
+            spanCount = 4;
         }
         layoutManager = new GridLayoutManager(this, spanCount);
         recyclerView.setLayoutManager(layoutManager);
@@ -118,6 +146,9 @@ public class MainActivity extends AppCompatActivity implements ItemListener {
 
     @OnClick(R.id.submit)
     public void onSubmit() {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
         INDEX = 1;
         query = searchET.getText().toString().trim();
         year = yearET.getText().toString().trim();
@@ -137,11 +168,11 @@ public class MainActivity extends AppCompatActivity implements ItemListener {
     public void OnSearchResponse(SearchModel model) {
         if (model.response.equalsIgnoreCase("true")) {
             Log.d("SEARCH_RESPONSE", "Success");
-            List<ItemModel> list = new ArrayList<>(model.searchList);
-
+            list = new ArrayList<>(model.searchList);
             Log.d("SEARCH_RESPONSE", "List size: " + list.size());
             ItemAdapter adapter = new ItemAdapter(this, list);
             recyclerView.setAdapter(adapter);
+
             isLoading = true;
             INDEX++;
         }
@@ -157,6 +188,8 @@ public class MainActivity extends AppCompatActivity implements ItemListener {
             Log.d("LOAD_MORE_RESPONSE", "Success");
             ((ItemAdapter) recyclerView.getAdapter()).addNewList(model.searchList);
             recyclerView.getAdapter().notifyDataSetChanged();
+
+            list = new ArrayList<>(((ItemAdapter) recyclerView.getAdapter()).getList());
             Log.d("LOAD_MORE_RESPONSE", "New items added");
             isLoading = true;
             INDEX++;
@@ -168,6 +201,14 @@ public class MainActivity extends AppCompatActivity implements ItemListener {
 
     @Override
     public void OnError(String error) {
-        Snackbar.make(recyclerView, error, Snackbar.LENGTH_SHORT).show();
+        Snackbar.make(recyclerView, "Time out! " + error, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable(STATE_LIST, new ArrayList<>(list));
+        outState.putInt(STATE_INDEX, INDEX);
+        outState.putBoolean(STATE_LOADING, isLoading);
     }
 }
